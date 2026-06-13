9424132b34257bd9cc5591172c48bf4fe0900ca8
SarahBaez <zS23014136@estudiantes.uv.mx>
2026-06-13 17:12:12 -0600

    ya funciona

--- services/parking-service/src/main/java/mx/uv/sicae/parking/ParkingServiceApplication.java
+++ services/parking-service/src/main/java/mx/uv/sicae/parking/ParkingServiceApplication.java
@@ -3,10 +3,17 @@ package mx.uv.sicae.parking;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("mx.uv.sicae.parking.repository")
-public class ParkingServiceApplication {
+public class ParkingServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ParkingServiceApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ParkingServiceApplication.class, args);
